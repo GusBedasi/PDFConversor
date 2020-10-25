21b57ff886d97f520165f286d78c8f9840d7194e
GusBedasi <ggustavobedasi@gmail.com>
2020-10-25 02:42:08 -0300

    Add conversion to PDF

--- src/controller/PDFConversor.js
+++ src/controller/PDFConversor.js
@@ -1,7 +1,24 @@
+import PDFDocument from 'pdfkit'
+import fs from 'fs';
+import Path from 'path'
+
 export function toPDF (request, response) {
-    
-  const [files] = request.files
+  
+  // Get files from the request
+  const [fileObj] = request.files
 
-  console.log(files)
+  // Get filename by destructing
+  const { filename } = fileObj
 
+  // Filename without IMG extension
+  const filenameWithoutImgExtension = filename.split('.')[0] + '.pdf'
+
+  // Create a PDF
+  const doc = new PDFDocument()
+  doc.image(Path.resolve(__dirname, '..', '..', 'uploads', filename), 160, 20, { width: 300 })
+  doc.pipe(fs.createWriteStream(`pdf/${filenameWithoutImgExtension}`))
+  doc.end()
+
+  // Send PDF file to frontend
+  response.attachment(Path.resolve(__dirname, '..', '..', 'pdf', filenameWithoutImgExtension))
 }
--- web/main.js
+++ web/main.js
@@ -5,21 +5,8 @@ function sendImg() {
   // Take the file
   var file = document.getElementById('file').files[0]
 
-  let extension = null
-
-  if(file.name.split('.').length > 2) {
-    const lastArrayIndex = file.name.split('.').length - 1
-    console.log(lastArrayIndex)
-    // Take the file extension
-    extension = '.' + file.name.split('.')[lastArrayIndex]
-    console.log(extension)  
-  } else {
-    // Take the file extension
-    extension = '.' + file.name.split('.')[1]
-  }
-  
   // Check if there's a file
-  if (file.length == 0) {
+  if (file == undefined) {
     // Pop up an alert to warn the user
     Swal.fire({
       title: 'Erro!',
@@ -27,6 +14,20 @@ function sendImg() {
       icon: 'error',
       confirmButtonText: 'Ok'
     })
+
+    return
+  }
+
+  let extension = null
+
+  if(file.name.split('.').length > 2) {
+    // Ensure that the last index is the one used
+    const lastArrayIndex = file.name.split('.').length - 1
+    // Take the file extension
+    extension = '.' + file.name.split('.')[lastArrayIndex]
+  } else {
+    // Take the file extension
+    extension = '.' + file.name.split('.')[1]
   }
 
   // Create a formData to send the filename, file and file extension to the server
@@ -37,9 +38,15 @@ function sendImg() {
   fetch('http://localhost:3333/convert', {
     method: 'POST',
     body: formData
-  }).then(response => {
-    console.log(reponse)
-  }).then(response => {
-    console.log(response)
-  })
+  }).then(
+    cleanFields()
+  )
+}
+
+function cleanFields(){
+  const filename = document.getElementById('filename')
+  const file = document.getElementById('file')
+
+  filename.value = '';
+  file.value = '';
 }
