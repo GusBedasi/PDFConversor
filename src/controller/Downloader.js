604e38c67c9fbf069a7e2bc428cbde950f6a6594
GusBedasi <ggustavobedasi@gmail.com>
2020-10-26 08:30:26 -0300

    'fix: Downloads function change'

--- src/controller/Downloader.js
+++ src/controller/Downloader.js
@@ -1,15 +1,20 @@
 import Path from 'path'
+import fs from 'fs'
 
-export function downloadFile(request, response) {
+export async function downloadFile(request, response) {
 
   const { filename } = request.body
 
   response.download(Path.resolve(__dirname, '..', '..', 'pdf', filename), (err) => {
     if (err) {
       console.error(err)
-    }else {
-      console.log(`Download Feito ${filename}`)
     }
   })
-
+  
+  // Delete PDF file
+  /* fs.unlink(Path.resolve(__dirname, '..', '..', 'pdf', filename), (err) => {
+    if (err) {
+      console.error(err)
+    }
+  }) */
 }
